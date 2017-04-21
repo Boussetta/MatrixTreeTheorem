3817fb94d2d087431c9d7020b6c90e6e151a38a8
Wissem BOUSSETTA <boussetta.wissem@outlook.com>
2017-04-21 11:19:48 +0200

    update
    copyright text updated in Cayley.m and inc2deg.m files

--- Cayley.m
+++ Cayley.m
@@ -1,3 +1,23 @@
+%    Cayley.m
+
+%    Copyright © 2017 by Wissem Boussetta
+%
+%    This file is part of the Matrix-Tree Theorem package used to compute
+%    the number of spanning trees in directed or undirected graph.
+%
+%    Matrix-Tree Theorem package is free software: you can redistribute it 
+%    and/or modify it under the terms of the GNU General Public License as 
+%    published by the Free Software Foundation, either version 3 of the 
+%    License, or (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 function [ K ] = Cayley( in )
 % In mathematics, Cayley's formula is a result in graph theory named
 % after Arthur Cayley.
